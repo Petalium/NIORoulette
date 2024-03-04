caa1a8453c81c9e35ee43d750d590e603283c5e9
oids <oiders112@gmail.com>
2024-03-03 20:04:47 -0500

    Removed useless methods in PrintUtils

--- src/Roulette/utils/PrintUtils.java
+++ src/Roulette/utils/PrintUtils.java
@@ -1,25 +1,9 @@
 package Roulette.utils;
 
-import Roulette.Colors;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 public class PrintUtils {
-    public static void printClientConnected(String message) {
-
-    }
-
-    public static void printClientTerminated(String message) {
-
-    }
-
-    public static void printCommandSuccessful(String message) {
-
-    }
-
-    public static void printActionSuccessful(String message) {
-
-    }
 
     @Contract(value = "_, _ -> new", pure = true)
     public static @NotNull RuntimeException throwRunTimeException(String message, Exception e) {
